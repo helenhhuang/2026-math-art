68f6210590658a562976301fde15c51fcf8f6399
helenhhuang <helenh6006@gmail.com>
2026-02-09 12:25:35 -0500

    update 020926
    doesn't work

--- math_art.py
+++ math_art.py
@@ -4,29 +4,33 @@ import numpy as np
 
 fig, ax = plt.subplots()
 
-for value in range(1, 1000):
-    r = (1/150) + (1/10) * (np.sin((52*np.pi*value)/1000))**4
+#for value in range(1, 1000):
+#    r = (1/150) + (1/10) * (np.sin((52*np.pi*value)/1000))**4
+#
+#    x = np.cos((8*np.pi*value)/1000) * (
+#        1 - (1/2) * (np.cos((6*np.pi*value)/1000))**2
+#    )
+#
+#    y = np.sin((8*np.pi*value)/1000) * (
+#        1 - (1/2) * (np.cos((6*np.pi*value)/1000))**2
+#    )
+#
+#    circle = patches.Circle((x, y),
+#                            radius = r,
+#                            fill = False,
+#                            edgecolor = 'blue',
+#                            linewidth = 0.1
+#                        )
+#    
+#    ax.add_patch(circle)
 
-    x = np.cos((8*np.pi*value)/1000) * (
-        1 - (1/2) * (np.cos((67*np.pi*value)/1000))**2
-    )
-
-    y = np.sin((8*np.pi*value)/1000) * (
-        1 - (1/2) * (np.cos((67*np.pi*value)/1000))**2
-    )
-
-    circle = patches.Circle((x, y),
-                            radius = r,
-                            fill = False,
-                            edgecolor = 'blue',
-                            linewidth = 0.1
-                        )
+for theta in range(0, 3 * np.pi):
+    r = k * np.cos(2 - theta)
+    plt.plot(r)
     
-    ax.add_patch(circle)
-    
-ax.set_aspect('equal', adjustable = 'box')
-ax.set_xlim(-1, 1)
-ax.set_ylim(-1, 1)
+#ax.set_aspect('equal', adjustable = 'box')
+#ax.set_xlim(-1, 1)
+#ax.set_ylim(-1, 1)
 plt.show()
 
     
