2385bccceb97086ad6488b2789694ade903ee6d3
helenhhuang <helenh6006@gmail.com>
2026-02-15 15:09:20 -0500

    add staggered tree trunk

--- math_art.py
+++ math_art.py
@@ -2,36 +2,12 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 
-fig, ax = plt.subplots()
-
-#for value in range(1, 1000):
-#    r = (1/150) + (1/10) * (np.sin((52*np.pi*value)/1000))**4
-#
-#    x = np.cos((8*np.pi*value)/1000) * (
-#        1 - (1/2) * (np.cos((6*np.pi*value)/1000))**2
-#    )
-#
-#    y = np.sin((8*np.pi*value)/1000) * (
-#        1 - (1/2) * (np.cos((6*np.pi*value)/1000))**2
-#    )
-#
-#    circle = patches.Circle((x, y),
-#                            radius = r,
-#                            fill = False,
-#                            edgecolor = 'blue',
-#                            linewidth = 0.1
-#                        )
-#    
-#    ax.add_patch(circle)
-
-for theta in range(0, 3 * np.pi):
-    r = k * np.cos(2 - theta)
-    plt.plot(r)
+#Start with a single line
+#plt.vlines(0, -2, 2, linewidth = 10)
     
-#ax.set_aspect('equal', adjustable = 'box')
-#ax.set_xlim(-1, 1)
-#ax.set_ylim(-1, 1)
+#break line into smaller segments
+for ymin in np.arange(-2, 2, 0.5):
+    lw = 10-1.23 * (ymin + 2)
+    plt.vlines(0, ymin, ymin + 0.5, linewidth=lw)
+
 plt.show()
-
-    
-    
