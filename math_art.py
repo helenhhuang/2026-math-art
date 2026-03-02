cf3361929f5a459a2cd09da68bd425962cd5191a
helenhhuang <helenh6006@gmail.com>
2026-03-02 12:08:22 -0500

    randomly distribute trees

--- math_art.py
+++ math_art.py
@@ -1,13 +1,11 @@
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 import numpy as np
 
-#Start with a single line
-#plt.vlines(0, -2, 2, linewidth = 10)
-    
-#break line into smaller segments
-for ymin in np.arange(-2, 2, 0.5):
-    lw = 10-1.23 * (ymin + 2)
-    plt.vlines(0, ymin, ymin + 0.5, linewidth=lw)
+for _ in range(10):
+    x_pos = np.random.uniform(0, 10)
+    for ymin in np.arange(-2, 2, 0.5):
+        lw = 10-1.23 * (ymin + 2)
+        plt.vlines(x_pos, ymin - 0.1, ymin + 0.5, linewidth=lw)
 
+plt.xlim(-1, 11)
 plt.show()
